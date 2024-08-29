9e0015919043577e435c6dfbfb4c36dd44793727
kamol <xamidullayevsaidkamol@gmail.com>
2024-08-29 22:44:03 +0500

    finished site

---
+++ script.js
@@ -0,0 +1,31 @@
+const popUps = document.querySelectorAll('.popUp'),
+      popUpCloseBtns = document.querySelectorAll('.popUp-btn'),
+      form = document.querySelector('.form-box'),
+      Thanks = document.querySelector('.popUp-thanks'),
+      formBtn = document.querySelector('.form-btn');
+
+formBtn.addEventListener('click', () => {
+    Thanks.classList.remove('d-none')
+})
+popUpCloseBtns.forEach(close => {
+    close.addEventListener('click', () => {
+        close.parentElement.classList.add('hidePopUp')
+        setTimeout(() => {
+            close.parentElement.parentElement.classList.add('d-none');
+            window.location.reload()
+        },300)
+    })
+}) 
+window.addEventListener('click', (e) => {
+    popUps.forEach(popUp => {
+        if(e.target == popUp){
+            popUp.children[0].classList.add('hidePopUp')
+            setTimeout(() => {
+                popUp.classList.add('d-none');
+                window.location.reload()
+            },300)
+        }
+
+    })
+})
+ 
